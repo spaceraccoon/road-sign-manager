31c6bd43008f4bfa897e8d2077d34bccb1186b83
Ben <brosand@gmail.com>
2017-11-04 13:03:20 -0400

    about_page

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,9 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+/* GET about page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {title: 'Express'});
+});
 
 module.exports = router;
