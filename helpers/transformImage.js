5d1d115f7cf18ef29802945c16004db82516c396
yichern <yichern.tan@yale.edu>
2017-11-15 21:17:31 -0500

    completed image to binary string

--- helpers/transformImage.js
+++ helpers/transformImage.js
@@ -1,8 +1,7 @@
 var getPixels = require("get-pixels")
+var ndarray = require("ndarray")
 
 function transformImage(imageURL) {
-
-	var binString = "";
 
 	getPixels(imageURL, function(err, pixels) {
 		if (err) {
@@ -11,44 +10,39 @@ function transformImage(imageURL) {
 		}
 		console.log("got pixels", pixels.shape.slice());
 
-		var width = pixels.shape[0], height = pixels.shape[1]
+		var binString = "";
+		var image = "";
+		var threshold = 140;
+		var image_width = 96;
+		var image_height = 27;
 
-		for (var i = 0; i < width; i++) {
-			for (var j = 0; j < height; j++) {
-				rgb = pixels.get(i, j);
-				binString.concat((rgb & 0xff000000).toString());
+		var width = pixels.shape[0], height = pixels.shape[1]
+		console.log(pixels.shape);
+		for (var i = 0; i < image_height; i++) {
+			for (var j = 0; j < image_width; j++) {
+				rgb = [];
+				for (var k = 0; k < 3; k++) {
+						rgb.push(pixels.get(i, j, k));
+						// binString.concat((rgb & 0xff000000).toString());
+				}
+				if (rgb[0] < threshold || rgb[1] < threshold || rgb[2] < threshold)
+				{
+						binString += "1";
+				}
+				else
+				{
+						binString += "0";
+				}
 			}
 		}
-
+		console.log(binString.length);
+		return binString;
 	});
 
-
-
-  //
-	// var arrToConvert = [[0,0,1],[2,3,3],[4,4,5]];
-	// var newArr = [];
-  //
-  //
-	// for(var i = 0; i < arrToConvert.length; i++)
-	// {
-	//     newArr = newArr.concat(arrToConvert[i]);
-	// }
-  //
-	// console.log(newArr);
-	console.log(binString);
-
-	return binString;
 }
 
 module.exports = transformImage;
-
-// function whites(image) {
-//     return image & 0xff000000;
-// }
-//
-// function myFunction() {
-//     document.getElementById("demo").innerHTML = ages.filter(checkAdult);
-// }
-
+// https://secure.gravatar.com/avatar/6c96a0adccf3dfa55695b0fead298d7f?s=96&d=mm&r=pg
+// http://iconshow.me/media/images/xmas/standard-new-year-icons/128/Snowflake-icon.png
 // image = "https://pbs.twimg.com/profile_images/839721704163155970/LI_TRk1z_400x400.jpg"
 // transformImage(image);
