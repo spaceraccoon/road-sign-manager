a6c7c0a94a4350c09cab7ca987da4921ac735a91
Eugene <limzhiweieugene@gmail.com>
2017-11-21 18:20:21 -0500

    Align message POST routes

--- routes/message.js
+++ routes/message.js
@@ -22,28 +22,33 @@ router.get('/image', function(req, res, next) {
 });
 
 /* POST manual message and forwards binary string to road sign. */
-router.post('/manual', function(req, res, next) {
+router.post('/manual', async function(req, res, next) {
+  try {
+    // Check that req message matches regex [0-1]{2592} ie. binary string of length 2592
+    req.checkBody("message", "Please enter a valid binary string. See above for details.").matches(/^[0-1]{2592}$/, "i");
+    var errors = req.validationErrors();
 
-  // log the req message
-  console.log(req.body.message);
+    if (errors) {
+      throw(errors);
+    } else {
+      try {
+        await axios.post(process.env.ROAD_SIGN_URL, {
+          'message': req.body.message
+        });
+        res.render('messageManual', {
+          title: 'Manual Mode',
+          flash: { type: 'alert-success', messages: [ { msg: 'Success!' }]}
+        });
+      } catch (e) {
+        throw e;
+      }
+    }
+  } catch (e) {
+    console.log(e);
 
-  // Check that req message matches regex [0-1]{2592} ie. binary string of length 2592
-  req.checkBody("message", "Please enter a valid binary string. See above for details.").matches(/^[0-1]{2592}$/, "i");
-  var errors = req.validationErrors();
-  console.log(errors);
-
-  if (errors) {
-    res.render('messageManual', {
+    res.render('messageText', {
       title: 'Manual Mode',
-      flash: { type: 'alert-danger', messages: errors}
-    });
-  } else {
-    axios.post(process.env.ROAD_SIGN_URL, {
-      'message': req.body.message
-    });
-    res.render('messageManual', {
-      title: 'Manual Mode',
-      flash: { type: 'alert-success', messages: [ { msg: 'Success!' }]}
+      flash: { type: 'alert-danger', messages: errors }
     });
   }
 });
@@ -55,13 +60,13 @@ router.post('/text', async function(req, res) {
     try {
       await axios.post(process.env.ROAD_SIGN_URL, { message });
     } catch (e) {
-      console.log(e);
+      throw(e);
     }
     res.render('messageText', {
   		title: 'Text Mode',
   		flash: { type: 'alert-success', messages: [{ msg: 'Success!' }]}
   	});
-  } catch(e) {
+  } catch (e) {
     console.log(e);
 
     res.render('messageText', {
@@ -84,7 +89,7 @@ router.post('/image', async function(req, res) {
   		title: 'Image Mode',
   		flash: { type: 'alert-success', messages: [ { msg: 'Success!' }]}
   	});
-  } catch(e) {
+  } catch (e) {
     console.log(e);
 
     res.render('messageImage', {
