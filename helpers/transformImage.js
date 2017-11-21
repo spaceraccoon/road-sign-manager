4033337fbfdc82297a6bfff941fd6d1667df5dbe
Eugene <limzhiweieugene@gmail.com>
2017-11-21 18:01:46 -0500

    Refactored transformImage and aligned style

--- helpers/transformImage.js
+++ helpers/transformImage.js
@@ -1,72 +1,35 @@
-var getPixels = require("get-pixels")
-var ndarray = require("ndarray")
+const getPixels = require("get-pixels")
 
-// jpg test
-// https://secure.gravatar.com/avatar/90e9dac0543d142d0dbbfb1f6e9523c8?s=96&d=mm&r=pg
-
-// png test
-// http://icons.iconarchive.com/icons/graphicloads/100-flat-2/96/email-icon.png
-
-// function to export to message.js
 function transformImage(imageURL) {
-	return new Promise(function(fulfill, reject) {
-		pixelGetter(imageURL).then(function(res) {
-			console.log(res);
-			fulfill(res);
-		}).catch(function(err) {
-			reject(err);
+	return new Promise(function(resolve, reject) {
+		getPixels(imageURL, function(err, pixels) {
+			if (err) {
+				reject(err);
+			}	else {
+				resolve(processPixels(pixels));
+			}
 		});
 	});
 }
 
-// wrapper to get pixels of image at buffer
-function pixelGetter(imageURL) {
-	return new Promise(function(fulfill, reject) {
-		getPixels(imageURL, function(err, pixels) {
-			if (err) {
-				console.log("Bad image path");
-				reject(err);
-			}
-			else {
-				string = processPixels(pixels);
-				fulfill(string);
-			}
-		});
-	})
-}
-
-// function to process the pixels
-// pixels is represented in ndarray
+// Converts pixels into a binary string based on a color threshold
 function processPixels(pixels) {
-
-	// initialise parameters
-	var binString = "";
-	var threshold = 170;
-	var image_width = 96;
-	var image_height = 27;
+	var binString = '';
+	const threshold = 170;
 	var height_offset = 30;
 
-	// iterate over height and width
-	for (var i = height_offset; i < image_height + height_offset; i++) {
-		for (var j = 0; j < image_width; j++) {
-
-			// create list of the rgb triple
+	for (var i = height_offset; i < parseInt(process.env.DISPLAY_HEIGHT) + height_offset; i++) {
+		for (var j = 0; j < parseInt(process.env.DISPLAY_WIDTH); j++) {
 			rgb = [];
 			for (var k = 0; k < 4; k++) {
-					rgb.push(pixels.get(j, i, k));
+				rgb.push(pixels.get(j, i, k));
 			}
 
-			// set string to 1 if any rgb value below threshold and opacity not 0 (for png)
-			if ((rgb[0] < threshold || rgb[1] < threshold || rgb[2] < threshold) && rgb[3] != 0)
-			{
-					binString += "1";
-			}
-			else
-			{
-					binString += "0";
-			}
+			// Set string to 1 if any rgb value below threshold and opacity not 0 (for png)
+			binString += ((rgb[0] < threshold || rgb[1] < threshold || rgb[2] < threshold) && rgb[3] != 0) ? 1 : 0;
 		}
 	}
+
 	return binString;
 }
 
--- helpers/transformText.js
+++ helpers/transformText.js
@@ -1,36 +1,36 @@
-module.exports = function test(text) {
-	const jsdom = require("jsdom");
-	const { JSDOM } = jsdom;
-	const canvas = require('canvas-prebuilt');
+const jsdom = require('jsdom');
+const canvas = require('canvas-prebuilt');
 
+const { JSDOM } = jsdom;
+const { document } = (new JSDOM(`...`)).window;
 
-	const { document } = (new JSDOM(`...`)).window;
-
+function transformText(text) {
 	var buffer = document.createElement('canvas');
-
 	var bufContext = buffer.getContext('2d');
 
 	buffer.width = 96;
 	buffer.height = 27;
 
-	bufContext.fillStyle = "#FFFFFF";
+	bufContext.fillStyle = '#FFFFFF';
 	bufContext.fillRect(0, 0, 96, 27);
 
-	bufContext.textBaseline = "top";
-	bufContext.font = "12px verdana";
+	bufContext.textBaseline = 'top';
+	bufContext.font = '12px verdana';
 
-	bufContext.fillStyle = "#000000";
+	bufContext.fillStyle = '#000000';
 	bufContext.fillText(text, 0, 0);
 
 	var img = bufContext.getImageData(0, 0, 96, 27);
 
-	var answer = "";
+	var answer = '';
 	for (var y=0; y<27; y++) {
 		for (var x=0; x<96; x++) {
 			var whichPixel = 96*y + x;
-			answer += (img.data[4*whichPixel] < 140) ? 1 : 0; //If the red channel of the pixel is lower than 140, we consider it "on"
+			answer += (img.data[4*whichPixel] < 140) ? 1 : 0; //If the red channel of the pixel is lower than 140, we consider it 'on'
 		}
 	}
 
 	return answer;
 }
+
+module.exports = transformText;
