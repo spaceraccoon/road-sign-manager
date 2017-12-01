fdd26673aa3466d4a100dd04fa47d4ad647c0d8a
Annie <anniezhou301@users.noreply.github.com>
2017-12-01 12:03:10 -0500

    line by line transform

--- helpers/transformText.js
+++ helpers/transformText.js
@@ -4,32 +4,35 @@ const canvas = require('canvas-prebuilt');
 const { JSDOM } = jsdom;
 const { document } = (new JSDOM(`...`)).window;
 
-function transformText(text) {
+function transformText(lines) {
+	var binaryarr = [];
+	for (var i = 0; i<lines.length; i++){
 	var buffer = document.createElement('canvas');
 	var bufContext = buffer.getContext('2d');
-
+	var boxheight = lines.length;
 	buffer.width = 96;
-	buffer.height = 27;
+	buffer.height = 27/boxheight;
 
 	bufContext.fillStyle = '#FFFFFF';
-	bufContext.fillRect(0, 0, 96, 27);
+	bufContext.fillRect(0, 0, 96, 27/boxheight);
 
 	bufContext.textBaseline = 'top';
 	bufContext.font = '12px arial';
 	bufContext.fillStyle = '#000000';
-	bufContext.fillText(text, 0, 0);
+	bufContext.fillText(lines[i], 0, 0);
 
-	var img = bufContext.getImageData(0, 0, 96, 27);
+	var img = bufContext.getImageData(0, 0, 96, 27/boxheight);
 
 	var answer = '';
-	for (var y=0; y<27; y++) {
+	for (var y=0; y<27/boxheight; y++) {
 		for (var x=0; x<96; x++) {
 			var whichPixel = 96*y + x;
 			answer += (img.data[4*whichPixel] < 140) ? 1 : 0; //If the red channel of the pixel is lower than 140, we consider it 'on'
 		}
 	}
-
-	return answer;
+	binaryarr[i] = answer;
+	}
+	return binaryarr.join("");
 }
 
 module.exports = transformText;
