66ed97728d27e86ae0671ae3f46a94e6f0edb754
Annie <anniezhou301@users.noreply.github.com>
2017-12-01 12:07:16 -0500

    formatting

--- helpers/transformText.js
+++ helpers/transformText.js
@@ -1,36 +1,35 @@
 const jsdom = require('jsdom');
 const canvas = require('canvas-prebuilt');
-
 const { JSDOM } = jsdom;
 const { document } = (new JSDOM(`...`)).window;
 
 function transformText(lines) {
 	var binaryarr = [];
 	for (var i = 0; i<lines.length; i++){
-	var buffer = document.createElement('canvas');
-	var bufContext = buffer.getContext('2d');
-	var boxheight = lines.length;
-	buffer.width = 96;
-	buffer.height = 27/boxheight;
+		var buffer = document.createElement('canvas');
+		var bufContext = buffer.getContext('2d');
+		var boxheight = lines.length;
+		buffer.width = 96;
+		buffer.height = 27/boxheight;
 
-	bufContext.fillStyle = '#FFFFFF';
-	bufContext.fillRect(0, 0, 96, 27/boxheight);
+		bufContext.fillStyle = '#FFFFFF';
+		bufContext.fillRect(0, 0, 96, 27/boxheight);
 
-	bufContext.textBaseline = 'top';
-	bufContext.font = '12px arial';
-	bufContext.fillStyle = '#000000';
-	bufContext.fillText(lines[i], 0, 0);
+		bufContext.textBaseline = 'top';
+		bufContext.font = '12px arial';
+		bufContext.fillStyle = '#000000';
+		bufContext.fillText(lines[i], 0, 0);
 
-	var img = bufContext.getImageData(0, 0, 96, 27/boxheight);
+		var img = bufContext.getImageData(0, 0, 96, 27/boxheight);
 
-	var answer = '';
-	for (var y=0; y<27/boxheight; y++) {
-		for (var x=0; x<96; x++) {
-			var whichPixel = 96*y + x;
-			answer += (img.data[4*whichPixel] < 140) ? 1 : 0; //If the red channel of the pixel is lower than 140, we consider it 'on'
+		var answer = '';
+		for (var y=0; y<27/boxheight; y++) {
+			for (var x=0; x<96; x++) {
+				var whichPixel = 96*y + x;
+				answer += (img.data[4*whichPixel] < 140) ? 1 : 0; //If the red channel of the pixel is lower than 140, we consider it 'on'
+			}
 		}
-	}
-	binaryarr[i] = answer;
+		binaryarr[i] = answer;
 	}
 	return binaryarr.join("");
 }
