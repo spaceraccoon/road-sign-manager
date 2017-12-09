bbc52a1d97228a36c250001bbf5cae242d68216c
Isabella <isabella.teng@yale.edu>
2017-12-09 13:19:19 -0500

    ensure valid image url

--- routes/message.js
+++ routes/message.js
@@ -76,25 +76,65 @@ router.post('/text', async function(req, res) {
   }
 });
 
+// router.post('/manual', async function(req, res, next) {
+//   try {
+//     // Check that req message matches regex [0-1]{2592} ie. binary string of length 2592
+//     req.checkBody("message", "Please enter a valid binary string. See above for details.").matches(/^[0-1]{2592}$/, "i");
+//     var errors = req.validationErrors();
+//
+//     if (errors) {
+//       throw(errors);
+//     } else {
+//       try {
+//         await axios.post(process.env.ROAD_SIGN_URL, {
+//           'message': req.body.message
+//         });
+//         res.render('messageManual', {
+//           title: 'Manual Mode',
+//           flash: { type: 'alert-success', messages: [ { msg: 'Success!' }]}
+//         });
+//       } catch (e) {
+//         throw e;
+//       }
+//     }
+//   } catch (e) {
+//     console.log(e);
+//
+//     res.render('messageText', {
+//       title: 'Manual Mode',
+//       flash: { type: 'alert-danger', messages: errors }
+//     });
+//   }
+// });
+
 /* POST image URL message and forwards converted binary string to road sign. */
 router.post('/image', async function(req, res) {
   try {
-    let message = await transformImage(req.body.message);
-    try {
-      await axios.post(process.env.ROAD_SIGN_URL, { message });
-    } catch (e) {
-      throw e;
+    // Check that req message url is of type .png or .jpeg
+    req.checkBody("message", "Please enter a valid image URL, of type .png or .jpeg").matches("\*.png"|"\*.jpeg");
+    var errors = req.validationErrors();
+
+    if (errors) {
+      throw(errors);
+    } else {
+      let message = await transformImage(req.body.message);
+      try {
+        await axios.post(process.env.ROAD_SIGN_URL, { message });
+        console.log("made it here")
+        res.render('messageImage', {
+      		title: 'Image Mode',
+      		flash: { type: 'alert-success', messages: [ { msg: 'Success!' }]}
+      	});
+      } catch (e) {
+        throw e;
+      }
     }
-    res.render('messageImage', {
-  		title: 'Image Mode',
-  		flash: { type: 'alert-success', messages: [ { msg: 'Success!' }]}
-  	});
   } catch (e) {
     console.log(e);
 
     res.render('messageImage', {
       title: 'Image Mode',
-      flash: { type: 'alert-danger', messages: [ { msg: 'Failed to convert image!' }]}
+      flash: { type: 'alert-danger', messages: errors}
     });
   }
 });
