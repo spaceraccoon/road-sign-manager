8bff777ff7a19a1c72a33fa35af8d050b4852051
Eugene <limzhiweieugene@gmail.com>
2018-02-12 16:35:01 -0500

    Add ESLint and Prettier config

--- app.js
+++ app.js
@@ -1,25 +1,21 @@
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const validator = require('express-validator');
 
-require('dotenv').config()
+require('dotenv').config();
 
+const index = require('./routes/index');
+const message = require('./routes/message');
 
-var index = require('./routes/index');
-var message = require('./routes/message')
-
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -31,14 +27,14 @@ app.use('/', index);
 app.use('/message', message);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
